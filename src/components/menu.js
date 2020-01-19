03076c9fa787a9c86482cf9561f79f0d05f88bd7
Alex Khab <finko85@gmail.com>
2020-01-19 03:04:48 +0300

    added app menu toggling

--- src/components/menu.js
+++ src/components/menu.js
@@ -6,7 +6,7 @@ const createMenuMarkup = (items) => {
   return items.map((item) => {
     const activeClass = item.isActive ? ACTIVE_MENU_CLASS : ``;
     return (
-      `<a class="trip-tabs__btn ${activeClass}" href="#">
+      `<a class="trip-tabs__btn ${activeClass}" id="${item.title}" href="#">
         ${item.title}
       </a>`
     );
@@ -31,5 +31,28 @@ export default class SiteMenu extends AbstractComponent {
 
   getTemplate() {
     return createMenuTemplate(this._menuItems);
+  }
+
+  setActiveItem(selectedItem) {
+    this.getElement().querySelectorAll(`.trip-tabs__btn`)
+      .forEach((item) => {
+        if (item.id === selectedItem) {
+          item.classList.add(ACTIVE_MENU_CLASS);
+        } else {
+          item.classList.remove(ACTIVE_MENU_CLASS);
+        }
+      });
+  }
+
+  setChangeHandler(handler) {
+    this.getElement()
+      .addEventListener(`click`, (evt) => {
+        if (evt.target.tagName !== `A`) {
+          return;
+        }
+        const menuItem = evt.target.id;
+
+        handler(menuItem);
+      });
   }
 }
